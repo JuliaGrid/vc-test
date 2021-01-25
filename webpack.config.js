0ee339efb5459a102b6ad02b92555d9d77b1a4fc
Гридунова Юлия Сергеевна <ygridunova@tsc.ts>
2021-01-25 16:05:39 +0300

    Сборка проекта

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const path = require("path");
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+
+module.exports = {
+    entry: "./src/index.js",
+    output: {
+        path: path.resolve("./src/", "app"),
+        filename: "bundle.js",
+    },
+    module: {
+        rules: [
+            {
+                test: /\.less$/,
+                use: ExtractTextPlugin.extract({
+                    fallback: "style-loader",
+                    use: ["css-loader", "less-loader"],
+                }),
+            },
+        ],
+    },
+    plugins: [new ExtractTextPlugin("style.css")],
+};
