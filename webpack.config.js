fff984ffe29e22aa69709b52651c7afe4d149523
Гридунова Юлия Сергеевна <ygridunova@tsc.ts>
2021-01-26 18:50:25 +0300

    Изменена папка сборки

--- webpack.config.js
+++ webpack.config.js
@@ -2,21 +2,21 @@ const path = require("path");
 const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 module.exports = {
-    entry: "./src/index.js",
-    output: {
-        path: path.resolve("./src/", "app"),
-        filename: "bundle.js",
-    },
-    module: {
-        rules: [
-            {
-                test: /\.less$/,
-                use: ExtractTextPlugin.extract({
-                    fallback: "style-loader",
-                    use: ["css-loader", "less-loader"],
-                }),
-            },
-        ],
-    },
-    plugins: [new ExtractTextPlugin("style.css")],
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve("./", "docs"),
+    filename: "bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.less$/,
+        use: ExtractTextPlugin.extract({
+          fallback: "style-loader",
+          use: ["css-loader", "less-loader"],
+        }),
+      },
+    ],
+  },
+  plugins: [new ExtractTextPlugin("style.css")],
 };
