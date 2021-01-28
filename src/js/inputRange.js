c43187c3c66bd92435599579b4649498d27f7f68
Гридунова Юлия Сергеевна <ygridunova@tsc.ts>
2021-01-28 21:24:08 +0300

    Тест прокрутки на телефоне

--- src/js/inputRange.js
+++ src/js/inputRange.js
@@ -105,6 +105,10 @@ slider.onmouseup = function () {
   final.scrollIntoView({ behavior: "smooth" });
 };
 
-slider.touchend = function () {
-  final.scrollIntoView({ behavior: "smooth" });
-};
+slider.addEventListener(
+  "touchend",
+  function () {
+    final.scrollIntoView({ behavior: "smooth" });
+  },
+  false
+);
