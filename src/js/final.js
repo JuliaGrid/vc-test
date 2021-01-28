334ec7dd25063d85a695cd41aeb4eb22c857bf0a
Гридунова Юлия Сергеевна <ygridunova@tsc.ts>
2021-01-28 21:45:25 +0300

    При открытии одной сноски, другая закрывается

--- src/js/final.js
+++ src/js/final.js
@@ -5,15 +5,41 @@ let bedNote = document.querySelector("#final__bed");
 let depositNote = document.querySelector("#final__deposit");
 let bankNote = document.querySelector("#final__bank");
 
+function closeBed() {
+  bedNote.style.display = "none";
+  bedButton.style.background = "#FEEBEF";
+  bedButton.style.color = "#FE4D4A";
+}
+
+function closeDeposit() {
+  depositNote.style.display = "none";
+  depositButton.style.background = "#FEEBEF";
+  depositButton.style.color = "#FE4D4A";
+}
+
+function closeBank() {
+  bankNote.style.display = "none";
+  bankButton.style.background = "#FEEBEF";
+  bankButton.style.color = "#FE4D4A";
+}
+
 function openCloseBedNote() {
   if (window.getComputedStyle(bedNote).getPropertyValue("display") == "none") {
+    if (
+      window.getComputedStyle(depositNote).getPropertyValue("display") ==
+      "block"
+    ) {
+      closeDeposit();
+    } else if (
+      window.getComputedStyle(bankNote).getPropertyValue("display") == "block"
+    ) {
+      closeBank();
+    }
     bedNote.style.display = "block";
     bedButton.style.background = "#FE4D4A";
     bedButton.style.color = "#FFFFFF";
   } else {
-    bedNote.style.display = "none";
-    bedButton.style.background = "#FEEBEF";
-    bedButton.style.color = "#FE4D4A";
+    closeBed();
   }
 }
 
@@ -21,25 +47,40 @@ function openCloseDepositNote() {
   if (
     window.getComputedStyle(depositNote).getPropertyValue("display") == "none"
   ) {
+    if (
+      window.getComputedStyle(bedNote).getPropertyValue("display") == "block"
+    ) {
+      closeBed();
+    } else if (
+      window.getComputedStyle(bankNote).getPropertyValue("display") == "block"
+    ) {
+      closeBank();
+    }
     depositNote.style.display = "block";
     depositButton.style.background = "#FE4D4A";
     depositButton.style.color = "#FFFFFF";
   } else {
-    depositNote.style.display = "none";
-    depositButton.style.background = "#FEEBEF";
-    depositButton.style.color = "#FE4D4A";
+    closeDeposit();
   }
 }
 
 function openCloseBankNote() {
   if (window.getComputedStyle(bankNote).getPropertyValue("display") == "none") {
+    if (
+      window.getComputedStyle(bedNote).getPropertyValue("display") == "block"
+    ) {
+      closeBed();
+    } else if (
+      window.getComputedStyle(depositNote).getPropertyValue("display") ==
+      "block"
+    ) {
+      closeDeposit();
+    }
     bankNote.style.display = "block";
     bankButton.style.background = "#FE4D4A";
     bankButton.style.color = "#FFFFFF";
   } else {
-    bankNote.style.display = "none";
-    bankButton.style.background = "#FEEBEF";
-    bankButton.style.color = "#FE4D4A";
+    closeBank();
   }
 }
 
